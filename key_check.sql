9a8dc3367c28533619fe42d24320eb1663ab154f
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-09-10 15:28:20 -0600

    Added check

--- key_check.sql
+++ key_check.sql
@@ -29,7 +29,8 @@ CREATE TABLE `movie` (
   `director` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
   `release_year` date DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `movie_un` (`title`,`director`,`release_year`)
+  UNIQUE KEY `movie_un` (`title`,`director`,`release_year`),
+  CONSTRAINT `movie_check` CHECK (`runtime` >= 30)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -56,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-09-10 15:16:21
+-- Dump completed on 2022-09-10 15:27:59
